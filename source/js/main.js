983ea28e6e889103ed6306bd1dea12e6f4737624
uzinok <uzinok@yandex.ru>
2019-01-06 20:38:13 +0400

    06.01.19 переход на нубук, начинаю делать слайдер

--- source/js/main.js
+++ source/js/main.js
@@ -3,10 +3,14 @@ $(document).ready(function () {
   $(".page-header")[0].classList.remove("page-header--nojs");
 
   $(".page-header__button-nav-toggle").click(function () {
-    var block_nav = $(".page-header__block-nav")[0];
+    var block_nav = $(".page-header__block-nav")[0],
+      nav_toggle = this;
 
     block_nav.classList.toggle('page-header__block-nav--closed');
     block_nav.classList.toggle('page-header__block-nav--opened');
+
+    nav_toggle.classList.toggle('page-header__button-nav-toggle--closed');
+    nav_toggle.classList.toggle('page-header__button-nav-toggle--opened');
   });
 
 });
