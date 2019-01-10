06bc28cfdf11d2230cb7864d199a356840b33af1
uzinok_HP <uzinok@yandex.ru>
2019-01-11 01:55:07 +0400

    исправил слайдер: теперь на странице может находиться несколько слайдеров, не вызывает ошибки скрип без слайдера

--- source/js/main.js
+++ source/js/main.js
@@ -1,50 +1,55 @@
 $(document).ready(function () {
-  // page-header
+  // PAGE-HEADER
   $(".page-header")[0].classList.remove("page-header--nojs");
-
   $(".page-header__button-nav-toggle").click(function () {
     var block_nav = $(".page-header__block-nav")[0],
       nav_toggle = this;
-
     block_nav.classList.toggle('page-header__block-nav--closed');
     block_nav.classList.toggle('page-header__block-nav--opened');
-
     nav_toggle.classList.toggle('page-header__button-nav-toggle--closed');
     nav_toggle.classList.toggle('page-header__button-nav-toggle--opened');
   });
-  // slider
-  $(".slider")[0].classList.remove("slider--nojs");
-
-  var slides = document.querySelectorAll('.slider__item');
-  var currentSlide = 0;
-  document.querySelectorAll('.slider')[0].style.height = slides[currentSlide].offsetHeight + "px";
-
-  function nextSlide() {
-    goToSlide(currentSlide + 1);
-  }
-
-  function previousSlide() {
-    goToSlide(currentSlide - 1);
-  }
-
-  function goToSlide(n) {
-    slides[currentSlide].classList.toggle('slider__item--active');
-    currentSlide = (n + slides.length) % slides.length;
-    document.querySelectorAll('.slider')[0].style.height = slides[currentSlide].offsetHeight + "px";
-    slides[currentSlide].classList.toggle('slider__item--active');
-  }
-
-  var next = document.querySelectorAll('.slider__next')[0];
-  var previous = document.querySelectorAll('.slider__previous')[0];
-
-  next.onclick = function () {
-    nextSlide();
-  };
-  previous.onclick = function () {
-    previousSlide();
-  };
-// ALERT
+  // END PAGE-HEADER
+  // ALERT
   $(".alert__btn--js").click(function () {
     this.parentNode.style.display = "none";
   });
+  // END ALERT
+  // SLIDER
+  var slider = $(".slider");
+  for (var n = 0; n < slider.length; n++) {
+    my_slider(slider[n]);
+  };
+
+  function my_slider(slider) {
+    slider.classList.remove("slider--nojs");
+    var slider_item = slider.querySelectorAll(".slider__item");
+    var currentSlide = 0;
+    slider.style.height = slider_item[currentSlide].offsetHeight + "px";
+    var next = slider.querySelector('.slider__next');
+    var previous = slider.querySelector('.slider__previous');
+    next.onclick = function () {
+      nextSlide();
+    };
+    previous.onclick = function () {
+      previousSlide();
+    };
+
+    function nextSlide() {
+      goToSlide(currentSlide + 1);
+    }
+
+    function previousSlide() {
+      goToSlide(currentSlide - 1);
+    }
+
+    function goToSlide(n) {
+      slider_item[currentSlide].classList.toggle('slider__item--active');
+      currentSlide = (n + slider_item.length) % slider_item.length;
+      slider.style.height = slider_item[currentSlide].offsetHeight + "px";
+      slider_item[currentSlide].classList.toggle('slider__item--active');
+    }
+  };
 });
+
+// END SLIDER
