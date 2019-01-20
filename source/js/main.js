c0089607a4e2182717ca1dd0249863e9ef3102fe
uzinok_HP <uzinok@yandex.ru>
2019-01-21 01:08:23 +0400

    набросал паралакс на главной, нужно оптимизировать и отремонтировать стрелку на главной...

--- source/js/main.js
+++ source/js/main.js
@@ -101,31 +101,24 @@ $(document).ready(function () {
   var up_down_arrow = $(".up-down-arrow__btn")[0];
 
   $(".up-down-arrow__btn").click(function () {
-    switch (up_down_arrow.classList.contains("up-down-arrow__btn--up")) {
-      case true:
-        var pageY = window.pageYOffset || document.documentElement.scrollTop;
-        var PAGE_Y = pageY;
-        console.log(PAGE_Y);
-        up_down_arrow.classList.remove("up-down-arrow__btn--up");
-        // up_down_arrow.classList.add("up-down-arrow__btn--down");
-        $("html, body").animate({
-          scrollTop: $("body").offset().top
-        }, 500);
-        break;
-        // case false:
-        //   console.log(PAGE_Y);
-        //   $("html, body").animate({
-        //     scrollTop: $("body").offset().PAGE_Y
-        //   }, 500);
-        //   up_down_arrow.classList.add("up-down-arrow__btn--up");
-        //   up_down_arrow.classList.remove("up-down-arrow__btn--down");
-        //   break;
-    }
+    var pageY = window.pageYOffset || document.documentElement.scrollTop;
+    var PAGE_Y = pageY;
+    console.log(PAGE_Y);
+    up_down_arrow.classList.remove("up-down-arrow__btn--up");
+    $("html, body").animate({
+      scrollTop: $("body").offset().top
+    }, 500);
   });
 
   window.onscroll = function () {
     var pageY = window.pageYOffset || document.documentElement.scrollTop;
     var innerHeight = document.documentElement.clientHeight;
+
+    // function paralax(elem_paralax) {
+    //   var index_paralax = 2;
+    //   var height_paralax = document.documentElement.offsetHeight / index_paralax;
+    //   console.log(pageY + " " + innerHeight + " " + OffsetHeight + " " + height_paralax);
+    // }
     if (pageY > innerHeight) {
       if (!up_down_arrow.classList.contains("up-down-arrow__btn--up")) up_down_arrow.classList.add("up-down-arrow__btn--up");
     } else if (pageY < innerHeight) {
