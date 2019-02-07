8613fe2dae8a21646137e0f627342a02640e737f
uzinok <uzinok@yandex.ru>
2019-02-07 15:06:56 +0400

    07.02.19 небольшая пауза на этой сборке, делаю новую сборку для этого сайта

---
+++ source/js/demo_js.js
@@ -0,0 +1,48 @@
+/* <script src="js/TweenMax.min.js"></script>
+    <script src="js/pubsub.js"></script> */
+var targets = document.querySelectorAll('.block');
+
+var tl = new TimelineMax();
+
+tl.to("#y", 0.1, {
+  opacity: 0,
+  yoyo: true,
+  repeat: 3
+});
+
+tl.fromTo("#mask_rect_all", 1, {
+  y: 350,
+  height: 40
+}, {
+  y: 0,
+  height: 487
+});
+
+tl.to("#mask_rect", 1, {
+  x: -235,
+});
+
+// PubSub.subscribe("go-letter-go", function () {
+//   alert("go letter go");
+// })
+
+var options = {
+  rootMargin: '0px',
+  threshold: [1.0, 0.3, 0.5, 0.7]
+}
+
+var observer = new IntersectionObserver(items => {
+  items.forEach(el => {
+    if (el.isIntersecting && el.intersectionRatio > 0.7) {
+
+      el.target.classList.add("block-active");
+      // PubSub.publish("go-letter-go");
+
+    } else {
+      el.target.classList.remove("block-active");
+    }
+  })
+}, options);
+for (var i = 0; i < targets.length; i++) {
+  observer.observe(targets[i]);
+}
